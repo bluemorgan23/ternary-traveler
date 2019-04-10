d5f44baa8248e724ad6720b08e4d0a4818027816
Chris Morgan <bluemorgan2323@gmail.com>
2019-04-10 11:25:45 -0500

    Made the buildEachInterest function that creates the html elements from an object that will be passed into it.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
 import API from "./dataManager"
+import htmlForEachInterest from "./HTMLforEachInterest"
 
 console.log(API.getInterestsWithPlace());
+const mainContainer = document.querySelector("#display-container");
+API.getInterestsWithPlace().then(interestList => interestList.forEach(interest => mainContainer.appendChild(htmlForEachInterest.buildEachInterest(interest))))
